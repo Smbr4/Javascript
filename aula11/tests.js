bdb1e003cd020d5b2218826f0d1d126fef983bda
Vitor Furtado Araújo <167136308+Smbr4@users.noreply.github.com>
2025-09-10 21:39:11 -0300

    Update tests.js

--- aula11/tests.js
+++ aula11/tests.js
@@ -1,1 +1,8 @@
-alert('Nossa mensagem!')
+const num1= prompt('Digite um Número: ');
+const num2= prompt('Digite um Número: ');
+const resultado = num1 + num2;
+num1 =  parseFloat(num1);
+num2 = parseFloat(num2);
+
+
+alert('O resultado foi ',resultado);
