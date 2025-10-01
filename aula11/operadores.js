ead15dd77519b7d160f3d18ec7133d9917d76cc9
Vitor Furtado Araújo <167136308+Smbr4@users.noreply.github.com>
2025-10-01 11:33:20 -0300

    Update operadores.js

---
+++ aula11/operadores.js
@@ -0,0 +1,16 @@
+let num1 = 12.4566;
+let num2 = 2;
+
+//aqui não estou alterando o tipo da variavel num1
+//console.log(num1.toString()+num2);
+//console.log(typeof num1)
+
+//agora o valor vai ser alterado
+
+//num1 = num1.toString();
+//console.log(num1.toFixed(2))
+//console.log(number.isIteger(num1))
+
+//let temp = num1* 'ola';
+//verificando se uma conta é invalida
+//console.log (Number.isNaN(temp));
