bb063780269275255bcc0c6c3aac2edf987ba063
Vitor Furtado Araújo <167136308+Smbr4@users.noreply.github.com>
2025-10-02 13:33:05 -0300

    Create java.js

---
+++ aula17/java.js
@@ -0,0 +1,27 @@
+const pesso1a = {
+    nome :'vitor',
+    sobrenome:'furtado',
+    idade: 25,
+    
+    Fala() {
+        console.log(`${this.nome} ${this.sobrenome} esta falando Oii...`)
+    }
+};  
+
+document.innerhtml()
+pesso1a.Fala();
+// const pessoa2 = {
+//      nome02: 'maria',
+//      sobrenome02: 'oliveira',
+//     idade02: 55
+// };
+
+// function criarpessoa(nome, sobrenome, idade){
+//     return{
+//         nome,
+//         sobrenome,
+//         idade
+//     }
+// }
+
+// const pessoa = criarpessoa('Adelia', 'furtado');
